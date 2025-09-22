21aa5d01eb60bdcc8fc4c3928542c6e2cb81b5e4
olimac2606 <c.a.m.i.l.o.2.6@outlook.com>
2025-09-22 17:45:41 -0500

    Rutas de las vistas, establecidas, trabajando en menú lateral izquierdo

---
+++ storageManagerFrontend/src/assets/components/SideBar.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom"
+import { Button } from "@chakra-ui/react"
+
+export default function SideBar() {
+    return (
+        <aside>
+            <div className="flex items-center gap-2 p-4">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#4ade80" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-store h-8 w-8 text-primary"><path d="m2 7 4.41-4.41A2 2 0 0 1 7.83 2h8.34a2 2 0 0 1 1.42.59L22 7"></path><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path><path d="M15 22v-4a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2v4"></path><path d="M2 7h20"></path><path d="M22 7v3a2 2 0 0 1-2 2a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 16 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 12 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 8 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 4 12a2 2 0 0 1-2-2V7"></path></svg>
+                <span className="text-lg font-bold">StoreManager</span>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>
+                            <Button bg="#4ade80">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-layout-dashboard h-4 w-4"><rect width="7" height="9" x="3" y="3" rx="1"></rect><rect width="7" height="5" x="14" y="3" rx="1"></rect><rect width="7" height="9" x="14" y="12" rx="1"></rect><rect width="7" height="5" x="3" y="16" rx="1"></rect></svg>
+                                Dashboard
+                            </Button>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    )
+}
