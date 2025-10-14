bc86753fce52412bade489862ca8d56fa847410d
Olimac2606 <c.a.m.i.l.o.2.6@outlook.com>
2025-10-14 10:15:29 -0500

    Cambio de nombre de la carpeta frontend, 3 peticiones a la api funcionando correctamente.

--- frontend/src/assets/pages/Products.tsx
+++ frontend/src/assets/pages/Products.tsx
@@ -4,11 +4,12 @@ import { Input, InputGroup } from "@chakra-ui/react"
 import SearchIcon from "../utils/icons/SearchIcon"
 import SelectChakra from "../components/SelectChakra"
 import ProductsTable from "../components/Products/ProductsTable"
-import { useState, useMemo } from "react"
+import { useState, useMemo, useEffect } from "react"
 import DialogChakra from "../components/DialogChakra"
 import ProductForm from "../components/Products/ProductForm"
 import type { CategoryValue, Option, ProductFormType, ProductCategory, StatusValue, Product } from "@/types/product"
 import { useEditingProduct } from "../contexts/EditingProductContext"
+import axios from "axios"
 
 export default function Products() {
 
@@ -29,49 +30,12 @@ export default function Products() {
     "Actions",
   ]
 
-  const [products, setProducts] = useState<Product[]>(
-    [
-      { "id": 1, "name": "Wireless Headphones", "category": "Electronics", "price": 129.99, "stock": 45, "status": "In Stock" },
-      { "id": 2, "name": "Cotton T-Shirt", "category": "Clothing", "price": 24.99, "stock": 120, "status": "In Stock" },
-      { "id": 3, "name": "Coffee Maker", "category": "Home & Garden", "price": 89.99, "stock": 12, "status": "Medium" },
-      { "id": 4, "name": "Running Shoes", "category": "Sports", "price": 159.99, "stock": 3, "status": "Low Stock" },
-
-      { "id": 5, "name": "Bluetooth Speaker", "category": "Electronics", "price": 79.99, "stock": 56, "status": "In Stock" },
-      { "id": 6, "name": "4K Monitor 27\"", "category": "Electronics", "price": 329.0, "stock": 18, "status": "Medium" },
-      { "id": 7, "name": "Smartwatch Series X", "category": "Electronics", "price": 219.0, "stock": 6, "status": "Medium" },
-      { "id": 8, "name": "USB-C Hub 7-in-1", "category": "Electronics", "price": 39.99, "stock": 95, "status": "In Stock" },
-      { "id": 9, "name": "Noise-Canceling Earbuds", "category": "Electronics", "price": 99.99, "stock": 4, "status": "Low Stock" },
-
-      { "id": 10, "name": "Slim Fit Jeans", "category": "Clothing", "price": 49.99, "stock": 80, "status": "In Stock" },
-      { "id": 11, "name": "Classic Hoodie", "category": "Clothing", "price": 39.99, "stock": 22, "status": "In Stock" },
-      { "id": 12, "name": "Leather Belt", "category": "Clothing", "price": 19.99, "stock": 15, "status": "Medium" },
-      { "id": 13, "name": "Sneakers Lifestyle", "category": "Clothing", "price": 69.99, "stock": 7, "status": "Medium" },
-      { "id": 14, "name": "Summer Dress", "category": "Clothing", "price": 44.5, "stock": 5, "status": "Low Stock" },
-
-      { "id": 15, "name": "Air Fryer XL", "category": "Home & Garden", "price": 149.99, "stock": 25, "status": "In Stock" },
-      { "id": 16, "name": "Vacuum Cleaner Pro", "category": "Home & Garden", "price": 189.99, "stock": 9, "status": "Medium" },
-      { "id": 17, "name": "Memory Foam Pillow", "category": "Home & Garden", "price": 29.99, "stock": 70, "status": "In Stock" },
-      { "id": 18, "name": "Ceramic Cookware Set", "category": "Home & Garden", "price": 129.0, "stock": 14, "status": "Medium" },
-      { "id": 19, "name": "LED Floor Lamp", "category": "Home & Garden", "price": 59.99, "stock": 6, "status": "Medium" },
-      { "id": 20, "name": "Bamboo Cutting Board", "category": "Home & Garden", "price": 24.0, "stock": 3, "status": "Low Stock" },
-
-      { "id": 21, "name": "Yoga Mat Pro", "category": "Sports", "price": 39.99, "stock": 60, "status": "In Stock" },
-      { "id": 22, "name": "Adjustable Dumbbells (Pair)", "category": "Sports", "price": 249.0, "stock": 11, "status": "Medium" },
-      { "id": 23, "name": "Resistance Bands Set", "category": "Sports", "price": 29.99, "stock": 85, "status": "In Stock" },
-      { "id": 24, "name": "Cycling Helmet", "category": "Sports", "price": 79.99, "stock": 8, "status": "Medium" },
-      { "id": 25, "name": "Tennis Racket Graphite", "category": "Sports", "price": 139.0, "stock": 4, "status": "Low Stock" },
-      { "id": 26, "name": "Soccer Ball Match", "category": "Sports", "price": 34.99, "stock": 33, "status": "In Stock" },
-
-      { "id": 27, "name": "Electric Kettle", "category": "Home & Garden", "price": 39.99, "stock": 21, "status": "In Stock" },
-      { "id": 28, "name": "Men's Polo Shirt", "category": "Clothing", "price": 27.99, "stock": 48, "status": "In Stock" },
-      { "id": 29, "name": "Portable SSD 1TB", "category": "Electronics", "price": 119.0, "stock": 16, "status": "Medium" },
-      { "id": 30, "name": "Gaming Mouse RGB", "category": "Electronics", "price": 49.99, "stock": 58, "status": "In Stock" },
-      { "id": 31, "name": "Trail Running Jacket", "category": "Sports", "price": 99.99, "stock": 2, "status": "Low Stock" },
-      { "id": 32, "name": "Velvet Throw Blanket", "category": "Home & Garden", "price": 54.99, "stock": 40, "status": "In Stock" }
-    ]
-  )
+  const [products, setProducts] = useState<Product[]>([])
   const [category, setCategory] = useState<CategoryValue>("all")
   const [inputValue, setInputValue] = useState("")
+  const [onPost, setOnPost] = useState<Product | undefined>(undefined)
+  const [onPatch, setOnPatch] = useState<Product | undefined>(undefined)
+  const [onDeleteProduct, setOnDeleteProduct] = useState<number>(0)
   const categoryMap: Record<Exclude<CategoryValue, "all">, ProductCategory> = {
     electronics: "Electronics",
     clothing: "Clothing",
@@ -101,8 +65,12 @@ export default function Products() {
     if (editingProduct !== null) {
       if (editingProduct.id === unshiftToProducts.id) {
         setProducts(prev => prev.filter((product) => product.id !== editingProduct.id))
+        setProducts(prev => [unshiftToProducts, ...prev])
+        setOnPatch(unshiftToProducts)
+        return
       }
     }
+    setOnPost(unshiftToProducts)
     setProducts(prev => [unshiftToProducts, ...prev])
   }
 
@@ -133,9 +101,61 @@ export default function Products() {
     setProducts(
       prev => prev.filter(product => product.id !== idProduct)
     )
+    setOnDeleteProduct(idProduct)
   }
 
   const { editingProduct } = useEditingProduct()
+
+  useEffect(() => {
+    if (onDeleteProduct === 0) return
+
+    const deleteProduct = async () => {
+      try {
+        await axios.delete(`http://localhost:3000/products/${onDeleteProduct}`)
+      } catch (error) {
+        console.log("Error deleting product", error)
+      }
+    }
+
+    deleteProduct()
+  })
+
+  useEffect(() => {
+    if (!onPatch) return
+    const patchProducts = async () => {
+      try {
+        await axios.patch(`http://localhost:3000/products/${onPatch?.id}`, onPatch)
+      } catch (error) {
+        console.log("Error updating product", error)
+      }
+    }
+    patchProducts()
+  }, [onPatch])
+
+  useEffect(() => {
+    if (!onPost) return
+    const postProducts = async () => {
+      try {
+        await axios.post(`http://localhost:3000/products/`, onPost)
+      } catch (error) {
+        console.log("Error creating product", error)
+      }
+    }
+    postProducts()
+  }, [onPost])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/products/")
+        setProducts(res.data)
+      } catch (error) {
+        console.log("Error getting the products", error)
+      }
+    }
+
+    fetchProducts()
+  }, [])
   return (
     <div className="px-[1.8rem] pt-[1rem] bg-[#FAFAFA]">
       <div className="flex items-center justify-between">
